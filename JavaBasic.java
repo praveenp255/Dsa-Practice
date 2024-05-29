c351d8b2084d19e59f054e1656b460ef68c709b6
praveen <praveenpatel050@gmail.com>
2024-05-30 00:39:58 +0530

    new files

---
+++ JavaBasic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class JavaBasic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+    float red = sc.nextFloat();
+    float area = 3.14f * red * red;
+    System.out.println(area);
+
+
+
+    }     
+}
